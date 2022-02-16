2a7dc065e12e47b9e76f2e86dadede07418f4b39
Samruddhi Thakor <sammythakor123@gmail.com>
2022-02-16 12:04:08 +0530

    Image Compression_2

--- JPEG/main.py
+++ JPEG/main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import os
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +11,10 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    os.system('python zigzag.py')
+    os.system('python Compressor.py')
+    os.system('python decompressor.py')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
